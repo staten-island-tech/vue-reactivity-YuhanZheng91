c469755260c4eb3ee8df96889eafbd76dc9a1de1
yuhanz1 <yuhanz9149@gmail.com>
2024-02-09 10:12:29 -0500

    rtged

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -4,19 +4,20 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+    <h1> Dokkaebi Bag </h1>
+    <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
   </header>
 </template>
 
 <style scoped>
-
+h1{
+  text-align: center;
+  font-weight: bold;
+}
 </style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1> Dokkaebi Bag</h1>
+    <div class="container" >
+    <h2> {{ items.name }}</h2>
+    </div>
   </div>
 </template>
 
@@ -11,82 +13,87 @@ const items = [
     desc: "A gadget without an owner. A strange, weird human-made gadget. Its purpose is unknown.",
     image: "",
     imageDesc: "a gadget with a black surface",
-    price: "1,000,000,000 c"
+    price: 1000000000,
   },
   {
     name: "Broken Egg Shells",
-    desc: "Egg shells without much value. The shells belong to a baby dokkaebi who hatched recently.",
+    desc: "Egg shells without much value. The shells belong to a baby dokkaebi who hatched years ago.",
     image: "",
-    imageDesc: "Yellow egg shells that are broken.",
-    price: "9158 c",
+    imageDesc: "Yellow egg shells that are broken and cracked.",
+    price: 9158,
   },
   {
     name: "Broken Faith",
     desc: "A sword with the former purpose of destroying the world. It broke in the process.",
     image: "",
     imageDesc: "A bright double-edged sword with a silver hilt, welding the properties of divinity, dark, and fire.",
-    price: "999,999,999 c",
+    price: 999999999,
   },
   {
     name: "Elf's Elixir",
     desc: "A potion that restores wounds.",
     image: "",
     imageDesc: "A round potion bottle containing light green liquid",
-    price: "5000 c",
+    price: 5000,
   },
   {
     name: "Artifically Flavored Lemon Lollipop",
     desc: "A lollipop meant for the purpose of eliminating exhaustion and restores alertness. The flavor is the best as well.",
     image: "",
     imageDesc: "A lollipop with yellow candy. Despite being artificially flavored, it tastes really good.",
-    price: "5100 c",
+    price: 5100,
   },
   {
     name: "Random Weapon Box",
     desc: "Want SSSS-grade weapons? Here it is! Buy one and get a random weapon with a great chance of getting a legendary weapon.",
     image: "",
     imageDesc: "",
-    price: "1500 c"
+    price: 1500,
   },
   {
     name: "Random Skill Box",
     desc: "Want SSSS-grade skill? Here it is! Buy one and get a random skill with a great chance of getting a legendary skill.",
     image: "",
     imageDesc: "",
-    price: "1500 c"
+    price: 1500,
   }, 
   {
     name: "Random Armor Box",
     desc: "Want SSSS-grade armor? Here it is! Buy one and get a random armor with a great chance of getting a legendary armor.",
     image: "",
     imageDesc: "",
-    price: "1500 c"
+    price: 1500,
   }, 
   {
     name: "Recovery Package",
     desc: "Recover from injury and fatigue!",
     image: "",
     imageDesc: "",
-    price: "1000 c"
+    price: 1000,
   },
   {
     name: "Sword of Judgement",
     desc: "A sword used by the strongest woman loved by many. The judge of destruction shall return and reclaim what is hers.",
     image: "",
     imageDesc: "A longsword with a large red-colored eye between the hilt and blade. The blade is black.",
-    price: "1,000,000 c",
+    price: 1000000,
   },
   {
     name: "The Present",
     desc: "An abandoned, custom-made pocket watch. A reminder of the present to a dear regressor.",
     image: "",
-    imageDesc: "A grey pocket watch with 'Aileen' carved on the bottom of it.",
-    price: "1,000,000 c",
+    imageDesc: "A silver pocket watch with 'Aileen' carved on the bottom of it.",
+    price: 5049,
   },
   {
-    name: ""
-  }
+    name: "A Writer's Diary",
+    desc: "A notebook belonging to a particular author, who wrote and waited more than 50 years for just that one reader. Where has he gone?",
+    image: "",
+    imageDesc: "A hardcover diary; it has a black cover and white pages. Each page is filled with a foreign language.",
+    price: 4951,
+  },
 ];
+
 </script>
 
 <style scoped>
